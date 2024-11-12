e9456402a8f4e2f8bfc15038ed73d67d579040b6
shirel-maymon <shirelmaymon@hilma.tech>
2024-11-12 14:41:36 +0200

    dbjson

---
+++ shirel/src/DBjson.js
@@ -0,0 +1,20 @@
+const DataBaceJson = [
+  {
+    userName: "hodaya762",
+    email: "hodaya@email.com",
+    phoneNumber: 4745734671,
+    id: 1,
+  },
+  {
+    userName: "shirel",
+    email: "shirel@email.com",
+    phoneNumber: 7548568365,
+    id: 2,
+  },
+  {
+    userName: "shlomo",
+    email: "shlomo@email.com",
+    phoneNumber: 1234567890,
+    id: 3,
+  },
+];
